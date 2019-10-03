d44acf2e28ca8c0dfaa502b45b549e4af6fa6f51
casey-cowens <cassandracowens93@gmail.com>
2019-10-03 11:27:24 -0400

    more working javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+let categoryOne = document.getElementsByClassName('category-1');
+for(let i=0; i < categoryOne.length; i++){
+    categoryOne[i].addEventListener('click', () => {
+        alert('hello')
+    })
+}
+
+let categoryTwo = document.getElementsByClassName('category-2');
+for(let i=0; i < categoryTwo.length; i++){
+    categoryTwo[i].addEventListener('click', () =>{
+        alert('hello')
+    })
+}
+
+let categoryThree = document.getElementsByClassName('category-3');
+for(let i=0; i < categoryThree.length; i ++){
+    categoryThree[i].addEventListener('click', () =>{
+        alert('hello')
+    })
+}
+
+let categoryFour = document.getElementsByClassName('category-4');
+for (let i=0; i <categoryFour.length; i++){
+    categoryFour[i].addEventListener('click', () =>{
+    alert('hello')
+    })
+}
+
+let categoryFice = document.getElementsByClassName('category-5');
+for (let i=0; i < categoryFive.length; i++){
+    categoryFive[i].addEventListener('click', ()=>{
+    alert('hello')
+})
+}
+
+//<INPUT TYPE="button" VALUE="Back" onClick="history.go(-1);"></INPUT>
